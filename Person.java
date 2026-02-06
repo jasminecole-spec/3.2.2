954912f459de37f7edabc61585fe6070b19b70db
Jasmine <jasmine.cole@crpusd.org>
2026-02-06 09:21:37 -0800

    constructors

--- Person.java
+++ Person.java
@@ -1,7 +1,36 @@
 public class Person
 {
-  public static void main(String[] args)
-  {
-    
-  }
+    private int age = 0;
+    private String name;
+    private double height;
+
+    public void setAge(int a)
+    {
+      age = a;
+    }
+
+    public void setName(String n)
+    {
+      name = n;
+    }
+
+    public void setHeight(double h)
+    {
+      height = h;
+    }
+
+    public int getAge()
+    {
+      return age;
+    }
+
+    public String getName()
+    {
+      return name;
+    }
+
+    public double getHeight()
+    {
+      return height;
+    }
 }
